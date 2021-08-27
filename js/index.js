bb8a2d5e305a27747ad945d57cfb8ac2d2e5df4f
MLMridul <mdmridul344@gmail.com>
2021-08-27 20:26:38 +0600

    Jquery Plugin Add

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+// Preloader Js
+
 $(document).ready(function () {
     $('.preloader').delay(2500).fadeOut();
 });
+
+// ScrollTop Js
+
+$(window).scroll(function () {
+    var scrooling = $(this).scrollTop();
+    if (scrooling > 400) {
+        $('.scrollTop').fadeIn();
+    } else {
+        $('.scrollTop').fadeOut();
+    }
+
+    if (scrooling > 350) {
+        $('.menu-bg').addClass('bg');
+    } else {
+        $('.menu-bg').removeClass('bg');
+    }
+})
+
+$('.scrollTop i').click(function () {
+    $('html ,body').animate({
+        scrollTop: '0',
+    }, 1500)
+})
